f0f99ef8e5d69bbac1709cd2ca7296edded10d05
Sethu Nair <sethunair2102@gmail.com>
2023-06-12 11:10:25 -0600

    assignment queries added

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT sum(assignment_submissions.duration)
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name  = 'FEB12';
+
+
