f4f5d9c09fa91439e3b5e54bba324392448d0acf
Sethu Nair <sethunair2102@gmail.com>
2023-06-13 15:51:12 -0600

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,14 +10,15 @@ const pool = new Pool({
 const input = process.argv.splice(2);
 const month = input[0];
 const limit = input[1];
+const values = [`%${month}%`, limit];
 
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${month}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -9,6 +9,7 @@ const pool = new Pool({
 
 const input = process.argv.splice(2);
 const month = input[0];
+const values = [month];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -16,9 +17,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${month}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
